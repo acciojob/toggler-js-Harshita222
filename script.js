b8e273bd99898045cbc8712c1214a6895e07f3d7
Harshita222 <124767756+Harshita222@users.noreply.github.com>
2024-08-08 14:01:58 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,20 +1,9 @@
-const checkboxes = document.querySelectorAll('.toggle');
-let checked = [];
-checkboxes.forEach((checkbox) => {
-    checkbox.addEventListener('click', function() {
-        // your logic here
-		if(checkbox.checked){
-			checked.push(checkbox);
-			if(checked.length >2){
-				checked[0].checked = false;
-				checked.shift();
-			}else{
-				const index = checked.indexOf(checkbox);
-				if (index > -1) {
-                checked.splice(index, 1);
-            
-			}
-		}
-		
-    });
-});
+        const toggles = document.querySelectorAll('.toggle');
+        toggles.forEach(toggle => {
+            toggle.addEventListener('change', () => {
+                const checkedToggles = document.querySelectorAll('.toggle:checked');
+                if (checkedToggles.length > 2) {
+                    checkedToggles[0].checked = false;
+                }
+            });
+        });
